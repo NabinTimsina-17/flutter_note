e8dcf9d445634c2ce47eaef87848f298d1d9020a
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2024-04-02 10:22:08 +0545

    note keeping

--- lib/screens/create_note.dart
+++ lib/screens/create_note.dart
@@ -1,19 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note/models/note_model.dart';
 
 class CreateNote extends StatefulWidget {
-  const CreateNote({super.key});
+  const CreateNote({super.key, required this.onNewNoteCreated});
+
+  final Function(Note) onNewNoteCreated;
 
   @override
   State<CreateNote> createState() => _CreateNoteState();
 }
 
 class _CreateNoteState extends State<CreateNote> {
+
+  final titleController =  TextEditingController();
+  final bodyControlller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Note'),
       ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: titleController,
+               style: const TextStyle(
+                fontSize: 28
+               ),
+               decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: "Title"
+               ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: bodyControlller,
+              style: const TextStyle(
+                fontSize: 21
+               ),
+               decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: "Your Story"
+               ),
+            )
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        if(titleController.text.isEmpty){
+          return;
+        }
+        if(bodyControlller.text.isEmpty){
+          return;
+        }
+
+        final note = Note(
+           body: bodyControlller.text,
+          title: titleController.text,
+        
+         );
+
+         widget.onNewNoteCreated(note);
+         Navigator.of(context).pop();
+      },
+
+      child: const Icon(Icons.save),
+      ) ,
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note/models/note_model.dart';
 import 'package:flutter_note/screens/create_note.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,19 +10,60 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Note> notes = List.empty(growable: true);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Notes"),
-    ),
-
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const CreateNote()));
-      },
-      child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    notes[index].title,
+                    style: const TextStyle(
+                       fontSize: 20
+                    ),
+                  ),
+                  const SizedBox(height: 10,),
+                   Text(
+                    notes[index].title,
+                    style: const TextStyle(
+                       fontSize: 20
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) =>  CreateNote(onNewNoteCreated: onNewNoteCreated,)));
+        },
+        child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void onNewNoteCreated(Note note) {
+    notes.add(note);
+    setState(() {});
+  }
+
+  void onNewDeleted(int index) {
+    notes.removeAt(index);
+    setState(() {});
   }
 }
